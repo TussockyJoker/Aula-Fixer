ad17674577a764475f0c652f460fcc441787bfec
Ugric <epicugric@gmail.com>
2026-02-26 21:18:39 +0000

    add .env support

--- build_scripts/build_ff_unlisted.sh
+++ build_scripts/build_ff_unlisted.sh
@@ -1,6 +1,10 @@
-cd ..
+#!/usr/bin/env bash
+
+source .env
+
+cd src
 mv manifest.json manifest_temp.json
 mv manifest_ff.json manifest.json
-web-ext sign --channel=unlisted --api-key=user: --api-secret=
+web-ext sign --channel=unlisted --api-key=user:$API_KEY --api-secret=$API_SECRET
 mv manifest.json manifest_ff.json
 mv manifest_temp.json manifest.json
