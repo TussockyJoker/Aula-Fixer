105ff6e0cd3599cf4d79ca0e8175fff665f25d1a
Tussocky Joker <cheshand@hotmail.co.uk>
2026-02-26 22:07:40 +0000

    Updated xpi for ff, updated manifests, updated build script

--- build_scripts/build_ff_unlisted.sh
+++ build_scripts/build_ff_unlisted.sh
@@ -8,3 +8,5 @@ mv manifest_ff.json manifest.json
 web-ext sign --channel=unlisted --api-key=user:$API_KEY --api-secret=$API_SECRET
 mv manifest.json manifest_ff.json
 mv manifest_temp.json manifest.json
+cd ..
+mv src/web-ext-artifacts/* builds/
