b4cfdd103be322174b870e201a949184cba332a2
Tussocky Joker <cheshand@hotmail.co.uk>
2026-03-02 13:29:19 +0000

    built 1.4.3, fixed listed build script

--- build_scripts/build_ff_listed.sh
+++ build_scripts/build_ff_listed.sh
@@ -6,6 +6,6 @@ echo "Building/signing listed firefox extension"
 mv src/manifest.json src/manifest_temp.json
 mv src/manifest_ff.json src/manifest.json
 echo "manifest_ff > manifest"
-web-ext sign --channel=unlisted --api-key=user:$API_KEY --api-secret=$API_SECRET --source-dir=./src --artifacts-dir=./builds
+web-ext sign --channel=listed --api-key=user:$API_KEY --api-secret=$API_SECRET --source-dir=./src --artifacts-dir=./builds
 mv src/manifest.json src/manifest_ff.json
 mv src/manifest_temp.json src/manifest.json
